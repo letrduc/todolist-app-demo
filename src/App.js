11209c9ba10b059477228d4a298a2775d37f83a7
Tran Duc <ducl6337@gmail.com>
2022-10-19 16:58:34 +0700

    Update Version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import "./App.css";
+import TaskList from "./components/TaskList";
+import AddTaskForm from "./components/AddTaskForm";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: "task_1", title: "Learn JS fundamental", status: 0 },
+    { id: "task_2", title: "Code a Todo List level 2", status: 0 },
+  ]);
+  const [showIncomplete, setShowIncomplete] = useState(true);
+  const [newTask, setNewTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0,
+      };
+      setTasks([...tasks, task]);
+      setNewTask("");
+    }
+  };
+
+  const handeleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const setTaskStatus = (taskID, status) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskID) {
+          return { ...task, status: status ? 1 : 0 };
+        }
+        return task;
+      })
+    );
+  };
+
+  const removeTask = (taskID) => {
+    setTasks(tasks.filter((task) => task.id !== taskID));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Todo List" subTitle="Get things done" />
+
+      <TaskList
+        tasks={tasks}
+        showIncomplete={showIncomplete}
+        setTaskStatus={setTaskStatus}
+        removeTask={removeTask}
+        setShowIncomplete={setShowIncomplete}
+      />
+
+      <AddTaskForm
+        newTask={newTask}
+        handleSubmit={handleSubmit}
+        handeleInputChange={handeleInputChange}
+      />
     </div>
   );
 }
